8039056a84f0bef3efbda7d7b7b73742a0000737
Christopher Bauer <chris@DEVIMAGE-LAPTOP.enterprisecb.com>
2018-12-27 22:15:54 -0500

    WIP getting users to attach to subreddit nodes

--- web/Map.js
+++ web/Map.js
@@ -7,12 +7,13 @@ var Map = function() {
         svg: null,
         graph: null,
         radius: 10,
+        linkGroup: null,
         createSubRedditNode: function(id) {
             this.graph.subreddits.push({ id: id, name: "r/"+id });
-            // console.log(this.graph.subreddits);
         },
         createUserNode: function(authorId, subredditId) {
             this.graph.users.push({ id: authorId });
+            this.graph.links.push({ source: subredditId, target: authorId });
         },
         init: function() {
             this.svg = d3.select("body")
@@ -30,14 +31,14 @@ var Map = function() {
 
             var map = d3.select(".map");
 
-            // var linkForce = d3.forceLink()
-            //     .id(link => link.id)
-            //     .distance(50)
-            //     .strength(link => .01);
+            var linkForce = d3.forceLink()
+                .id(link => link.id)
+                .distance(5)
+                .strength(link => .01);
             
             this.simulation = d3
                 .forceSimulation()
-                // .force("link", linkForce)
+                .force("link", linkForce)
                 .force("charge", d3.forceManyBody()
                     .strength(.2)
                     )
@@ -45,11 +46,11 @@ var Map = function() {
                 .force("center", d3.forceCenter(this.width / 2, this.height / 2))
                 ;
             
-            // this.linkGroup = map.append("g")
-            //     .attr("class", "links");
+            this.linkGroup = map.append("g")
+                .attr("class", "links");
             this.nodeGroup = map.append("g")
                 .attr("class", "nodes");
-            
+            this.nodeElements = this.nodeGroup.selectAll("g");
             this.updateSimulation();
         },
         dragDrop: null,
@@ -57,14 +58,13 @@ var Map = function() {
         nodeElements: null,
         linkGroup: null,
         nodeGroup: null,
-        updateGraph: function() {
-            this.nodeElements = this.nodeGroup.selectAll("g")
+        updateSubreddits: function() {
+            var elements = this.nodeGroup.selectAll("g.subreddit")
                 .data(this.graph.subreddits);
             
-            this.nodeElements.exit().remove();
-
-            var nodeEnter = this.nodeElements.enter()
+            var nodeEnter = elements.enter()
                 .append("g")
+                    .attr("class", "subreddit")
                     .attr("x", this.width / 2)
                     .attr("y", this.height / 2)
                     ;
@@ -78,30 +78,85 @@ var Map = function() {
             nodeEnter
                 .append("text")
                     .attr("y", -15)
-                    .attr("x", d => -((d.id.length * 12) / 4))
+                    .attr("x", d => -((d.name.length * 12) / 4))
                     .text(d => d.name)
                 ;
 
+            elements.exit().remove();
+
             this.nodeElements = nodeEnter.merge(this.nodeElements);
         },
-        updateSimulation: function() {    
-            this.updateGraph();
-            this.simulation.nodes(this.graph.subreddits).on("tick", () => {
+        updateUsers: function() {
+            var elements = this.nodeGroup.selectAll("g.user")
+                .data(this.graph.users);
             
+            var nodeEnter = elements.enter()
+                .append("g")
+                    .attr("class", "user")
+                    .attr("x", this.width / 2)
+                    .attr("y", this.height / 2)
+                    ;
+
+            nodeEnter
+                .append("circle")
+                    .attr("id", function(d) { return d.id; })
+                    .attr("data-id", d => d.id)
+                    .attr("stroke", "black")
+                    .attr("stroke-width", "1px")
+                    .attr("r", this.radius / 2);
+            
+            nodeEnter
+                .append("text")
+                    .attr("y", -15)
+                    .attr("x", d => -((d.id.length * 12) / 4))
+                    .text(d => d.id)
+                ;
+
+            elements.exit().remove();
+
+            this.nodeElements = nodeEnter.merge(this.nodeElements);
+        },
+        updateLinkElements: function() {
+            this.linkElements = this.linkGroup.selectAll("line")
+                .data(this.graph.links);
+        
+            this.linkElements.exit().remove();
+        
+            var linkEnter = this.linkElements
+                .enter().append("line")
+                    .attr("stroke-width", 1)
+                    .attr("stroke", "#e5e5e5");
+        
+            this.linkElements = linkEnter.merge(this.linkElements);
+        },
+        updateGraph: function() {
+            this.updateLinkElements();
+            this.updateSubreddits();
+            this.updateUsers();
+        },
+        updateSimulation: function() {
+            this.updateGraph();
+            var nodeSource = this.graph.subreddits.concat(this.graph.users);
+            
+            var simulation = this.simulation.nodes(nodeSource);
+            console.log({ subCount: this.graph.subreddits.length, userCount: this.graph.users.length, nodeSourceCount: nodeSource.length});
+            console.log(this.nodeElements._groups);
+
+            simulation.on("tick", () => {
                 this.nodeElements
                     .attr('transform', node => "translate(" + node.x + "," + node.y + ")")
                     ;
             
-                // this.linkElements
-                //     .attr("x1", link => link.source.x)
-                //     .attr("y1", link => link.source.y)
-                //     .attr("x2", link => link.target.x)
-                //     .attr("y2", link => link.target.y);
+                this.linkElements
+                    .attr("x1", link => link.source.x)
+                    .attr("y1", link => link.source.y)
+                    .attr("x2", link => link.target.x)
+                    .attr("y2", link => link.target.y);
             
             });
         
-            // this.simulation.force("link").links(this.graph.links);
-            this.simulation.alphaTarget(20).restart();
+            simulation.force("link").links(this.graph.links);
+            simulation.alphaTarget(20).restart();
         }
     }
 }();
